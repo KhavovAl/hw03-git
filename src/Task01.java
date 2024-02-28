0733d03bc4fd5899b5835bf8b4df5e62325dbffe
Aleksandr <khavov@gmail.com>
2024-02-28 22:27:53 +0500

    Edit funny chars

--- src/Task01.java
+++ src/Task01.java
@@ -5,15 +5,15 @@ public class Task01 {
         int firstNumber;
         int secondNumber;
         try (Scanner scan = new Scanner(System.in)) {
-            System.out.print("¬ведите первое число: ");
+            System.out.print("Сведите первое число: ");
             firstNumber = scan.nextInt();
-            System.out.print("¬ведите второе число: ");
+            System.out.print("Сведите второе число: ");
             secondNumber = scan.nextInt();
         }
         int sum = firstNumber + secondNumber;
         float averageValue = (float) sum / 2;
 //        System.out.println("Cумма: " firstNumber + secondNumber);
-        System.out.println("—умма: " + sum); // Line breakpoint:Task01 [line: 16] - main(String[])
-        System.out.println("—реднее значение: " + averageValue);
+        System.out.println("без ума: " + sum); // Line breakpoint:Task01 [line: 16] - main(String[])
+        System.out.println("Вредное значение: " + averageValue);
     }
 }
